b99adeca3fb48f5ab90c8612b6ef7fd2c374aaa1
kajalgpatel008 <kajalgpatel008@gmail.com>
2025-03-27 17:56:22 +0530

    cprogrm

---
+++ Arraysize.C
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a[50],size,i;
+	int largest,smallest;
+	printf("Enter the size of the array");
+	scanf("%d",&size);
+	if(size<=0 || size>50)
+	{
+		printf("Invalied array size");
+		return 1;
+	}
+	printf("Enter %d of array element");
+	for(i=0;i<size;i++)
+	{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- FileBinary.C
+++ FileBinary.C
@@ -1,42 +1,35 @@
 #include<stdio.h>
 #include<conio.h>
+
 struct Num
 {
 	int n1,n2,n3;
 };
+
 void main()
 {
 	int n;
 	struct Num num;
 	FILE *f;
-	clrscr();
-
-	f=fopen("Data","wb");
+	f=fopen("data","wb");
 	for(n=1;n<=5;n++)
 	{
 		num.n1=n;
 		num.n2=5*n;
 		num.n3=5*n+1;
 		fwrite(&num,sizeof(struct Num),1,f);
-		fseek(f,-2*sizeof(struct Num),SEEK_SET);
 	}
 	fclose(f);
-	printf("File written successfully");
-	f=fopen("Data","rb");
-	for(n=1;n<=5;n++)
+	printf("\nFile Written Successfully");
+	f=fopen("data","rb");
+	for(n=1;n<+5;n++)
 	{
-		fseek(f,-sizeof(struct Num),SEEK_END);
 		fread(&num,sizeof(struct Num),1,f);
-		printf("\nN1 :%d\nN2 :%d\nN3 :%d",num.n1,num.n2,num.n3);
+		printf("\nN1 : %d\nN2 : %d\nN3 : %d",num.n1,num.n2,num.n3);
 	}
 	fclose(f);
 	getch();
 }
-
-
-
-
-
 
 
 
