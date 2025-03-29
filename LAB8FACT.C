9dac30a0dadbb1101b0fd799e2004cf3dcc2c440
kajalgpatel008 <kajalgpatel008@gmail.com>
2025-03-29 17:37:58 +0530

    c program

--- LAB8FACT.C
+++ LAB8FACT.C
@@ -2,7 +2,7 @@
 #include<conio.h>
 void fact(int n)
 {
-	int i,f=1;
+	int i,f=1;       //fact/=1
 	for(i=1;i<=n;i++)
 	{
 		f=f*i;
@@ -14,7 +14,7 @@ void main()
 	clrscr();
 	printf("\nEnter number :");
 	scanf("%d",&no);
-	factorial=fact(no);
+	factorial=fact(n);
 	printf("The Factorial of %d is : %d",no,factorial);
 	getch();
 }
